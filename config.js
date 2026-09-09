9e5150f3111a031a0ed0115968e95f8a988aaef9
sachirainduwara-git <sachirainduwara239@gmail.com>
2026-09-09 21:29:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "mongodb+srv://sachirainduwara02_db_user:Sachi2010@cluster0.skykj4x.mongodb.net/?appName=Cluster0",
+  SESSION_ID: process.env.SESSION_ID || "",
   PREFIX: process.env.PREFIX || ".",
   OWNER_NUM: process.env.OWNER_NUM || "94760579211",
-  ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sachirainduwara/Bot/blob/main/images/SACHIYA%20MD.png?raw=true",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sachirainduwara-git/Sachiya-MD/blob/main/media/Image.jpg",
   MODE: process.env.MODE || "public"
 };
