96306f630ac2d3b6b289a08f5064dbc26da1d9a3
sachirainduwara-git <sachirainduwara239@gmail.com>
2026-09-09 22:40:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "mongodb+srv://sachirainduwara:<db_password>@whatsapp.um8jkiq.mongodb.net/?appName=Whatsapp",
   PREFIX: process.env.PREFIX || ".",
   OWNER_NUM: process.env.OWNER_NUM || "94760579211",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sachirainduwara-git/Sachiya-MD/blob/main/media/Image.jpg",
