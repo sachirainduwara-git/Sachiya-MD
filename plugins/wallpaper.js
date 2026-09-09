0d81cd7062dbe97ae00d7150d4ac9f60cc2deedd
sachirainduwara-git <sachirainduwara239@gmail.com>
2026-09-09 22:03:36 +0530

    Update wallpaper.js

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -47,7 +47,7 @@ cmd(
         from,
         {
           image: {
-            url: "https://github.com/sachirainduwara/Bot/blob/main/images/SACHIYA%20MD.png?raw=true",
+            url: "https://github.com/sachirainduwara-git/Sachiya-MD/blob/main/media/Image.jpg",
           },
           caption: header,
         },
