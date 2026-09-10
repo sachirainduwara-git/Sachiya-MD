0b3c32e2734691c357b9fe6541028e0335364136
sachirainduwara-git <sachirainduwara239@gmail.com>
2026-09-10 12:26:56 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -48,7 +48,7 @@ async(sachiya, mek, m, { from, quoted, pushname, reply }) => {
         // 1. Voice Note (Audio) එක Raw Link එක හරහා යැවීම
         await sachiya.sendMessage(from, {
             audio: { url: 'https://github.com/sachirainduwara-git/Sachiya-MD/raw/refs/heads/main/media/Bailalentho.mp3' },
-            mimetype: 'audio/ogg; codecs=opus',
+            mimetype: 'audio/mpeg',
             ptt: true
         }, { quoted: mek });
 
