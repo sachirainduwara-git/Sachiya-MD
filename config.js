32d741e4ce761bf8646f520db040711d89b3a89b
sachirainduwara-git <sachirainduwara239@gmail.com>
2026-09-12 15:58:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "mongodb+srv://sachirainduwara:sachira2010@whatsapp.um8jkiq.mongodb.net/?retryWrites=true&w=majority&appName=Whatsapp",
   PREFIX: process.env.PREFIX || ".",
   OWNER_NUM: process.env.OWNER_NUM || "94760579211",
-  ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sachirainduwara-git/Sachiya-MD/blob/main/media/Image.jpg",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sachirainduwara-git/Sachiya-MD/blob/main/media/IMG_0160.png?raw=true",
   MODE: process.env.MODE || "public"
 };
